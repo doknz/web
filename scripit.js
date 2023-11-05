0781a138912c658001879279d7532378fbd1fd84
doknz <143971484+doknz@users.noreply.github.com>
2023-11-05 19:10:35 +0300

    Update scripit.js

--- scripit.js
+++ scripit.js
@@ -16,3 +16,49 @@ document.getElementById("searchInput").addEventListener("input", function () {
         }
     }
 });
+
+
+
+
+// القائمة المكونة من الأخبار
+var newsList = [
+  "خبر 1: هذا هو الخبر الأول",
+  "خبر 2: هذا هو الخبر الثاني",
+  "خبر 3: هذا هو الخبر الثالث",
+  // يمكنك إضافة المزيد من الأخبار هنا
+];
+
+// متغير لتتبع الخبر الحالي
+var currentNewsIndex = 0;
+
+// عرض الخبر الأول عند بدء التحميل
+showCurrentNews();
+
+// انتظر حدوث الحدث "input" على مربع البحث
+document.getElementById("searchInput").addEventListener("input", function () {
+    // الحصول على قيمة مربع البحث
+    var searchTerm = this.value.toLowerCase();
+
+    // الحصول على العناصر الخاصة بالأخبار
+    var newsBox = document.getElementById("newsBox");
+
+    // عرض أو إخفاء الأخبار بناءً على البحث
+    if (newsList[currentNewsIndex].toLowerCase().includes(searchTerm)) {
+        newsBox.style.display = "block"; // إظهار العنصر
+    } else {
+        newsBox.style.display = "none"; // إخفاء العنصر
+    }
+});
+
+// عرض الخبر الحالي
+function showCurrentNews() {
+    var newsBox = document.getElementById("newsBox");
+    newsBox.innerText = newsList[currentNewsIndex];
+}
+
+// التبديل بين الأخبار
+document.getElementById("nextNewsButton").addEventListener("click", function () {
+    // زيادة الفهرس لعرض الخبر التالي
+    currentNewsIndex = (currentNewsIndex + 1) % newsList.length;
+    showCurrentNews();
+});
