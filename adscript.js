0a7d4755d2a7a106e7353779bdc1916e1c016d17
doknz <143971484+doknz@users.noreply.github.com>
2023-11-06 17:59:34 +0300

    Update adscript.js

--- adscript.js
+++ adscript.js
@@ -1,2 +1,9 @@
-var adDiv = document.getElementById('advertisement');
-adDiv.innerHTML = '<p>هذا هو الإعلان الخاص بنا! إعلان جديد هنا.</p>';
+// انشئ عنصر div لعرض الإعلان
+var adDiv = document.createElement('div');
+adDiv.id = 'advertisement'; // يمكنك تغيير هذا إلى الهوية أو الفئة التي تريدها
+
+// قم بتخصيص نص الإعلان والمحتوى
+adDiv.innerHTML = '<p>هذا هو الإعلان الخاص بنا!</p>';
+
+// أضف العنصر إلى صفحة الويب
+document.body.appendChild(adDiv);
