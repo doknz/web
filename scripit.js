e8a73e90f8134249f126eac41f4a2c62c9cd4c95
doknz <143971484+doknz@users.noreply.github.com>
2023-11-06 17:06:26 +0300

    Update scripit.js

--- scripit.js
+++ scripit.js
@@ -62,3 +62,24 @@ document.getElementById("nextNewsButton").addEventListener("click", function () 
     currentNewsIndex = (currentNewsIndex + 1) % newsList.length;
     showCurrentNews();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// انشئ عنصر div لعرض الإعلان
+var adDiv = document.createElement('div');
+adDiv.id = 'advertisement'; // يمكنك تغيير هذا إلى الهوية أو الفئة التي تريدها
+
+// قم بتخصيص نص الإعلان والمحتوى
+adDiv.innerHTML = '<p>هذا هو الإعلان الخاص بنا!</p>';
+
+// أضف العنصر إلى صفحة الويب
+document.body.appendChild(adDiv);
