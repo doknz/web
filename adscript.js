6f1635fbac9848c9a1f0730dd08c6b4fec211d6b
doknz <143971484+doknz@users.noreply.github.com>
2023-11-06 18:01:23 +0300

    Update adscript.js

--- adscript.js
+++ adscript.js
@@ -1,4 +1,11 @@
-// انشئ عنصر div لعرض الإعلان
+// السكريبت الأول
+var script1 = document.createElement('script');
+script1.src = "//pl21174538.toprevenuegate.com/b648da115a008d9509a7fac590057c1b/invoke.js";
+script1.async = true;
+script1.setAttribute('data-cfasync', 'false');
+document.body.appendChild(script1);
+
+// السكريبت الثاني
 var adDiv = document.createElement('div');
 adDiv.id = 'advertisement'; // يمكنك تغيير هذا إلى الهوية أو الفئة التي تريدها
 
